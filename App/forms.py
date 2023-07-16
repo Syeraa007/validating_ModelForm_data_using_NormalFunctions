3e98507ef87ad1798b3f4f43b4e4f5cdc44a673c
SYERAA007 <simhasimha4321.com>
2023-07-16 12:40:56 +0530

    first commit

--- App/forms.py
+++ App/forms.py
@@ -4,13 +4,23 @@ class TopicForm(forms.ModelForm):
     class Meta():
         model=Topic
         fields='__all__'
-        # help_text={'topic':'Enter a Topic'}
-        # labels={'topic':'Topic Name'}
+        help_texts={'topic':'Enter a valid Topic'}
+        labels={'topic':'Topic Name'}
+        
 class PageForm(forms.ModelForm):
     class Meta():
         model=Webpage
         fields='__all__'
+        # fields=['name','url']
+        # exclude=['url']
+        help_texts={'topic':'Enter a valid Topic','name':'Enter a valid Name','url': 'Enter a valid URL'}
+        labels={'topic':'Topic Name', 'name':'Name','url':'URL'}
+
 class RecordForm(forms.ModelForm): 
     class Meta():
         model=AccessRecord
         fields='__all__'
+        # fields=['name','author']
+        # exclude=['date']
+        help_texts={'name':'Enter a valid Name','date':'Enter a valid Date','author':'Enter a valid Author name'}
+        labels={'name':'Name','date':'Date','author':'Author'}
